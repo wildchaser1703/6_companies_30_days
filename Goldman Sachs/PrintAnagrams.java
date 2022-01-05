f428d403579bbccc0119f90059d296a6d5da5168
Toshali Mohapatra <toshalimohapatra1@gmail.com>
2022-01-05 13:56:59 +0530

    update gfg link

--- Goldman Sachs/PrintAnagrams.java
+++ Goldman Sachs/PrintAnagrams.java
@@ -1,3 +1,4 @@
+//https://practice.geeksforgeeks.org/problems/print-anagrams-together/1/#
 class PrintAnagrams{
     public List<List<String>> Anagrams(String[] string_list) {
         // Code here 
